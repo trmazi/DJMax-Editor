c6a207bf722eb2f6da20866eb873e28a36f65a59
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2023-12-08 19:35:47 -0500

    Update to latest .net.

--- DJMaxEditor/Resources.Designer.cs
+++ DJMaxEditor/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace DJMaxEditor {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1368,16 +1368,6 @@ namespace DJMaxEditor {
             get {
                 object obj = ResourceManager.GetObject("tempo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Byte[].
-        /// </summary>
-        public static byte[] Thumbs {
-            get {
-                object obj = ResourceManager.GetObject("Thumbs", resourceCulture);
-                return ((byte[])(obj));
             }
         }
         
